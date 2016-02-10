7bb72fa4b4312946964e9e558c1a24f64f08cba3
IMAN4K <iman72411@yahoo.com>
2016-02-10 20:46:36 +0330

    Update PushButton.cpp

--- Ui/Qt5.5.1/PushButton.cpp
+++ Ui/Qt5.5.1/PushButton.cpp
@@ -1,11 +1,11 @@
-#include "paint.h"
+#include "PushButton.h"
 
-paint::paint(QWidget *parent)
-	: QWidget(parent) {}
+PushButton::PushButton(QWidget *parent)
+	:QWidget(parent) {}
 
-paint::~paint() {}
+PushButton::~PushButton() {}
 
-void paint::paintEvent(QPaintEvent *e) {
+void PushButton::paintEvent(QPaintEvent *e) {
 	QPainter painter(this);
 	QPainterPath path;
 	path.addRect(QRect(0, 0, rect().width(), rect().height()));
@@ -21,29 +21,29 @@ void paint::paintEvent(QPaintEvent *e) {
 	painter.setFont(_fontfamily);
 }
 
-void paint::setText(QString Text) {
+void PushButton::setText(QString Text) {
 	_text = Text;
 }
 
-void paint::enterEvent(QEvent *) {
+void PushButton::enterEvent(QEvent *) {
 	back = "#E5F1FB";
 	border = "#0078D7";
 	repaint();
 }
 
-void paint::leaveEvent(QEvent *) {
+void PushButton::leaveEvent(QEvent *) {
 	back = "#E1E1E1";
 	border = "#ADADAD";
 	repaint();
 }
 
-void paint::mousePressEvent(QMouseEvent *) {
+void PushButton::mousePressEvent(QMouseEvent *) {
 	back = "#CDE4F7";
 	repaint();
 	emit clicked();
 }
 
-void paint::mouseReleaseEvent(QMouseEvent *) {
+void PushButton::mouseReleaseEvent(QMouseEvent *) {
 	back = "#E5F1FB";
 	repaint();
 }
