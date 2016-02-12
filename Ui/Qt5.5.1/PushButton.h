0d3e980e68b3135dbe04b4b3046724a7888a921e
IMAN4K <iman72411@yahoo.com>
2016-02-12 12:49:36 +0330

    Update PushButton.h

--- Ui/Qt5.5.1/PushButton.h
+++ Ui/Qt5.5.1/PushButton.h
@@ -7,30 +7,35 @@ class QPainter;
 class QPainterPath;
 
 
+
 class PushButton : public QWidget
 {
 	Q_OBJECT
 	Q_PROPERTY(QColor back_color READ back_color WRITE set_back_Color)
 	Q_PROPERTY(QColor border_color READ border_color WRITE set_border_Color)
+private:
+	enum State { Disable, Default, Normal };
+	QString _text, _fontfamily = "Calibri";
+	QColor _border = "#ADADAD", _back = "#E1E1E1", _color = "#000000";
+	int _pointsize = 12, _border_weight = 1;
+	void set_back_Color(QColor color);
+	void set_border_Color(QColor color);
+	QColor back_color();
+	QColor border_color();
+	State _state;
 public:
 	explicit PushButton(QString text = "", QWidget *parent = 0);
 	~PushButton();
 	void setText(QString Text);
 	QString text();
+	void setDefault(bool Default);
+	void setDisable(bool);
 protected:
 	void paintEvent(QPaintEvent *);
 	void enterEvent(QEvent *);
 	void leaveEvent(QEvent *);
 	void mousePressEvent(QMouseEvent *);
 	void mouseReleaseEvent(QMouseEvent *);
-private:
-	QString _text , _fontfamily = "Calibri";
-	QColor _border = "#ADADAD", _back = "#E1E1E1", _color = "#000000";
-	int _pointsize = 12;
-	void set_back_Color(QColor color);
-	void set_border_Color(QColor color);
-	QColor back_color();
-	QColor border_color();
 signals:
 	void clicked();
 };
