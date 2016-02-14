a25f518f5ace6bab7e9aade2310da1a8704ebec0
Iman <iman72411@yahoo.com>
2016-02-14 17:44:25 +0330

    Create FlatButton.h

---
+++ Ui/Qt5.5.1/FlatButton.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef PUSHBUTTON_H
+#define PUSHBUTTON_H
+
+#include <QtWidgets>
+class QPainter;
+class QPainterPath;
+class QPropertyAnimation;
+class QParallelAnimationGroup;
+
+
+class PushButton : public QWidget
+{
+	Q_OBJECT
+		Q_PROPERTY(QColor back_color READ back_color WRITE set_back_Color)
+		Q_PROPERTY(QColor border_color READ border_color WRITE set_border_Color)
+private:
+	enum State { Disable, Default, Normal };
+	QString _text, _fontfamily = "Open Sans";
+	QColor _border = "#ADADAD", _back = "#2FA9E2", _color = "#FFFFFF";
+	int _pointsize = 16, _border_weight = 1;
+	void set_back_Color(QColor color);
+	void set_border_Color(QColor color);
+	QColor back_color();
+	QColor border_color();
+	State _state = Normal;
+public:
+	explicit PushButton(QString text = "", QWidget *parent = 0);
+	~PushButton();
+	void setText(const QString Text);
+	QString text();
+	void setDefault(bool);
+	void setDisable(bool);
+protected:
+	virtual void paintEvent(QPaintEvent *);
+	virtual void enterEvent(QEvent *);
+	virtual void leaveEvent(QEvent *);
+	virtual void mousePressEvent(QMouseEvent *);
+	virtual void mouseReleaseEvent(QMouseEvent *);
+signals:
+	void clicked();
+};
+
+#endif // PUSHBUTTON_H
