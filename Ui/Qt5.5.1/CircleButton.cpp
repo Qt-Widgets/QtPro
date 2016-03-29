4d53d7e60bc115593e4f60140f8366d823407f68
Iman <iman72411@yahoo.com>
2016-03-29 11:47:07 +0430

    Update CircleButton.cpp

--- Ui/Qt5.5.1/CircleButton.cpp
+++ Ui/Qt5.5.1/CircleButton.cpp
@@ -1,1 +1,17 @@
+CircleButton::CircleButton(QWidget *parent)
+	: QWidget(parent)
+{}
 
+CircleButton::~CircleButton() {}
+
+void CircleButton::paintEvent(QPaintEvent *) {
+	QPainter painter(this);
+	painter.setRenderHint(QPainter::HighQualityAntialiasing);
+	painter.setBrush(QBrush(_back));
+	painter.setPen(Qt::NoPen);
+	painter.drawEllipse(rect());
+	painter.save();
+	QPixmap img("C:/Users/Zahra/Desktop/Arrow-1.png");
+	QRect center(rect().x(), rect().y() + _margin, rect().width(), rect().height() - (2 * _margin));
+	painter.drawPixmap(center , img);
+}
