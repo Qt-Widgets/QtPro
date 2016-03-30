6e8187e31e7bb599de5930b6a6c9a7bc36368d1f
Iman <iman72411@yahoo.com>
2016-03-30 20:52:03 +0430

    Update CircleButton.h

--- Ui/Qt5.5.1/CircleButton.h
+++ Ui/Qt5.5.1/CircleButton.h
@@ -14,12 +14,10 @@ public:
 	CircleButton(QWidget *parent = 0);
 	~CircleButton();
 private:
-	QColor _back = "#1ABC9C", _color="#FFFFFF";
-	qreal _opacity = 0.750;
-	QString _fontfamily = "Open Sans", _text = "Submit";
+	QColor _back = "#1ABC9C";
 	qint16 _pointsize = 12, _margin = 25, _blank = 150;
 	QTimer _timer_in, _timer_out;
-	QPixmap _image, _image1 = "C:/Users/Zahra/Desktop/Arrow-2.png", _image0 = "C:/Users/Zahra/Desktop/Arrow-1.png";
+	QPixmap _image, _image1 = "QtPro/Resources/Icons/Arrow-2.png", _image0 = "QtPro/Resources/Icons/Arrow-1.png";
 protected:
 	virtual void paintEvent(QPaintEvent *);
 	virtual void enterEvent(QEvent *);
