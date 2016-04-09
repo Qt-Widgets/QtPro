86cfc4a673d8789103773b52905f5a3beca2360e
Iman <iman72411@yahoo.com>
2016-04-09 15:21:36 +0430

    Update Button.h

--- Ui/Qt5.5.1/Button.h
+++ Ui/Qt5.5.1/Button.h
@@ -4,20 +4,27 @@
 
 #include <QtWidgets>
 class QPainter;
+enum State
+{
+	Normal,
+	Disable,
+	Hover,
+	Over
+};
 
 class Button : public QWidget
 {
 	Q_OBJECT
 private:
-	enum State { Disable, Default, Normal };
-	QString _text, _fontfamily = "Calibri";
+	QString _text, _fontfamily = "Open Sans";
 	int _pointsize = 12, _border_weight = 1;
 public:
 	explicit Button(const QString text = "", QWidget *parent = 0);
 	~Button();
 	void setText(const QString Text);
-	QString text() const ;
 	void setDisable(bool);
+	QString text() const;
+	State _state = Normal;
 protected:
 	virtual void paintEvent(QPaintEvent *);
 	virtual QSize minimumSizeHint() const;
@@ -26,4 +33,4 @@ signals:
 	void clicked();
 };
 
-#endif // PUSHBUTTON_H
+#endif // BUTTON_H
