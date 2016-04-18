327d2451b3edb9281ec38b611dfcf3f153a2c003
Iman <iman72411@yahoo.com>
2016-04-18 12:17:35 +0430

    Update TitleBar.h

--- Ui/Qt5.5.1/TitleBar.h
+++ Ui/Qt5.5.1/TitleBar.h
@@ -1,1 +1,20 @@
+#pragma once
+#ifndef TITLEBAR_H
+#define TITLEBAR_H
 
+#include <QtWidgets>
+
+class QWidget;
+
+class TitleBar : public QWidget
+{
+	Q_OBJECT
+
+public:
+	TitleBar(QWidget *parent = 0);
+	~TitleBar();
+protected:
+	virtual void mouseMoveEvent(QMouseEvent *e);
+};
+
+#endif // TITLEBAR_H
