bb931230adcf57015abd63bd1146694ad4bc7e43
Iman <iman72411@yahoo.com>
2016-04-18 12:18:00 +0430

    Update TitleBar.cpp

--- Ui/Qt5.5.1/TitleBar.cpp
+++ Ui/Qt5.5.1/TitleBar.cpp
@@ -1,1 +1,15 @@
+TitleBar::TitleBar(QWidget *parent)
+	: QWidget(parent)
+{
+	setWindowFlags(Qt::FramelessWindowHint);
+}
 
+TitleBar::~TitleBar() {}
+
+void TitleBar::mouseMoveEvent(QMouseEvent *e) {
+	if (e->buttons() && Qt::LeftButton)
+	{
+		move(e->globalPos() - e->pos());
+		e->accept();
+	}
+}
