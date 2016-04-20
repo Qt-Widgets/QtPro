c200d6655190290afc3083142f2c6887d1eb937e
Iman <iman72411@yahoo.com>
2016-04-20 14:57:04 +0430

    Update Window.h

--- Ui/Qt5.5.1/Window.h
+++ Ui/Qt5.5.1/Window.h
@@ -4,13 +4,20 @@
 
 #include <QtWidgets>
 class QWidget;
+class QPainter;
+class QGridLayout;
 
 class Window : public QWidget {
 	Q_OBJECT
 
+private:
+	QColor _background = "#FFFFFF";
+	QGridLayout _layout;
 public:
 	explicit Window();
 	~Window();
+protected:
+	virtual void paintEvent(QPaintEvent *);
 };
 
 #endif // !WINDOW_H
