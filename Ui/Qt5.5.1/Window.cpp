ffedb7256879a502e330cbaa7fa599c5afbf95f1
Iman <iman72411@yahoo.com>
2016-04-20 14:57:40 +0430

    Update Window.cpp

--- Ui/Qt5.5.1/Window.cpp
+++ Ui/Qt5.5.1/Window.cpp
@@ -1,5 +1,16 @@
 Window::Window() {
   setWindowFlags(Qt::FramelessWindowHint);
+  setWindowFlags(Qt::FramelessWindowHint);
+	_layout.setMargin(0);
+	_layout.setSpacing(0);
+	setLayout(&_layout);
 }
 
 Window::~Window() {}
+
+void Window::paintEvent(QPaintEvent *) {
+	QPainter painter(this);
+	painter.setBrush(_background);
+	painter.setPen(Qt::NoPen);
+	painter.drawRect(0, 0, width(), height());
+}
