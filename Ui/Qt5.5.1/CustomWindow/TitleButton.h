914700e92934db17bc3d41e39119050ca64168e1
Iman <iman72411@yahoo.com>
2016-04-21 12:40:33 +0430

    Update TitleButton.h

--- Ui/Qt5.5.1/CustomWindow/TitleButton.h
+++ Ui/Qt5.5.1/CustomWindow/TitleButton.h
@@ -17,6 +17,7 @@ enum Type
 class TitleButton : public QWidget
 {
 	Q_OBJECT
+
 private:
 	enum State
 	{
@@ -27,20 +28,27 @@ private:
 	};
 	State _state = Normal;
 	Type _type = Close;
-	QColor _color = "#B2E2E8";
+	QColor _color = "#FFFFFF";
 	qint16 _weight = 2;
-	qreal _opacity = 0.999;
+	qreal _opacity = 0.650, _step = 0.002;
 	QTimer _timer;
 	QPixmap _image = "C:/Users/IMAN/Downloads/media.png";
+
 public:
 	explicit TitleButton(Type _tp, QWidget *parent = 0);
 	~TitleButton();
+
 protected:
 	virtual void paintEvent(QPaintEvent *);
-	virtual QSize minimumSizeHint() const;
-	virtual QSize sizeHint() const;
 	virtual void enterEvent(QEvent *);
 	virtual void leaveEvent(QEvent *);
+	virtual void mousePressEvent(QMouseEvent *e);
+	virtual QSize minimumSizeHint() const;
+	virtual QSize sizeHint() const;
+
+	signals:
+	void clicked(Qt::MouseButton _button);
+
 public slots:
 	void timercall();
 
