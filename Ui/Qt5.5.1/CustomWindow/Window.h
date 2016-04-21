8fa8b04bf138a78668dd81e7206cdee47c476b2d
Iman <iman72411@yahoo.com>
2016-04-21 13:34:33 +0430

    Create Window.h

---
+++ Ui/Qt5.5.1/CustomWindow/Window.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QtWidgets>
+// #include /.../TitleBar.h
+
+class QWidget;
+class QPainter;
+class QGridLayout;
+
+class Window : public QWidget {
+	Q_OBJECT
+
+private:
+	QColor _background = "#FFFFFF";
+	QGridLayout _layout;
+	TitleBar _titlebar;
+public:
+	explicit Window();
+	~Window();
+protected:
+	virtual void paintEvent(QPaintEvent *);
+};
+
+#endif // !WINDOW_H
