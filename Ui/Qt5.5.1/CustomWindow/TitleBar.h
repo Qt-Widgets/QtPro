a8495355eae06ec96ba8b832e6ca2eeffff22943
Iman <iman72411@yahoo.com>
2016-04-28 11:07:55 +0430

    Update TitleBar.h

--- Ui/Qt5.5.1/CustomWindow/TitleBar.h
+++ Ui/Qt5.5.1/CustomWindow/TitleBar.h
@@ -9,25 +9,46 @@ class QWidget;
 class QPainter;
 class TitleButton;
 class QHBoxLayout;
+class QLabel;
 
 class TitleBar : public QWidget {
 	Q_OBJECT
 
 private:
+	QWidget *_parent;
 	QColor _background = "#00A0B4";//#1976D2
-	qint16 _width = 32;
-	QHBoxLayout *_l = new QHBoxLayout(), *_primary = new QHBoxLayout(), *_secoundry = new QHBoxLayout();
-	QLabel *_window_icon = new QLabel(), *_Title = new QLabel();
+	qint16 _width = 38;
+	qint16 _textwidth = 0;
+	QHBoxLayout *_l = new QHBoxLayout();
+	QHBoxLayout *_primary = new QHBoxLayout();
+	QHBoxLayout *_secoundry = new QHBoxLayout();
+	QLabel *_Title = new QLabel();
 	bool _hastext = 0;
+	bool _maximized = 0;
+	bool _hasicon = 0;
+	QPoint _start;
+	QPoint _delta;
+	QByteArray _state;
+	TitleWidget *_close = new TitleWidget(Close);
+	TitleWidget *_minimize = new TitleWidget(Minimize);
+	TitleWidget *_maxmimize = new TitleWidget(Maxmimize);
+	TitleWidget *_window_icon = new TitleWidget(Icon);
+
 public:
-	TitleButton *_close = new TitleButton(Close), *_minimize = new TitleButton(Minimize),
-		        *_maxmimize = new TitleButton(Maxmimize);
 	explicit TitleBar(QWidget *parent = 0);
 	~TitleBar();
 	void setText(const QString &_text);
 	void setIcon(const QPixmap &_pixelmap);
+	qint16 minimumwidth() const;
+
 protected:
 	virtual void paintEvent(QPaintEvent *);
+	virtual void mousePressEvent(QMouseEvent *e);
+	virtual void mouseMoveEvent(QMouseEvent *e);
+	virtual void mouseDoubleClickEvent(QMouseEvent *e);
+
+	public slots:
+	void changeState();
 };
 
 #endif // !TITLEBAR_H
