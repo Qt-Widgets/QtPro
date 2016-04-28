d5ce7bd4c3a12d2afe05d5712f915df4f220260f
Iman <iman72411@yahoo.com>
2016-04-28 20:23:53 +0430

    Update NcFramelessHelper.cpp

--- Ui/Qt5.5.1/NcFramelessHelper.cpp
+++ Ui/Qt5.5.1/NcFramelessHelper.cpp
@@ -1,23 +1,3 @@
-/*
-    FramelessHelper, an easy way to support move/resize on
-    frameless toplevel windows.
-    Copyright (C) 2011  Nishant Parashar
-    Email:- nishcode (at) gmail (dot) com
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <QRubberBand>
 #include <QMouseEvent>
 #include <QMutex>
@@ -45,7 +25,7 @@ public:
 
   static int mBorderWidth;
 };
-//TODO: Should not be static.
+
 int NcCursorPosCalculator::mBorderWidth = 5;
 
 class NcWidgetData
