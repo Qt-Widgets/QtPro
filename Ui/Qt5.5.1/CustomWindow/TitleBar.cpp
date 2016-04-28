5f7048f2d8e69cec6b8363ccaa4357ed0bddbf9b
Iman <iman72411@yahoo.com>
2016-04-28 11:09:01 +0430

    Update TitleBar.cpp

--- Ui/Qt5.5.1/CustomWindow/TitleBar.cpp
+++ Ui/Qt5.5.1/CustomWindow/TitleBar.cpp
@@ -1,16 +1,76 @@
-TitleBar::TitleBar(QWidget *parent)
-	:QWidget(parent)
+#include "TitleBar.h"
+
+TitleBar::TitleBar(QWidget *parent) 
+	:QWidget(parent), _parent(parent)
 {
-	_l->addStretch(10);
+	setFixedHeight(_width);
+	connect(_close, SIGNAL(clicked()), parent, SLOT(close()));
+	connect(_maxmimize, SIGNAL(clicked()), this, SLOT(changeState()));
+	connect(_minimize, SIGNAL(clicked()), parent, SLOT(showMinimized()));
+	_l->addStretch();
 	_l->addWidget(_minimize);
 	_l->addWidget(_maxmimize);
 	_l->addWidget(_close);
-	_l->setMargin(10);
-	_l->setSpacing((_width / 2) - 2);
+	_l->setMargin(0);
+	_l->setSpacing(0);
 	setLayout(_l);
 }
 
 TitleBar::~TitleBar() {}
+
+void TitleBar::setText(const QString &_text) {
+	delete _l;
+	_hastext = 1;
+	_textwidth += _text.size();
+
+	_Title->setText("  " + _text);
+	_Title->setMinimumHeight((_width / 2) + 3);
+	_Title->setStyleSheet("color:white;");
+	_Title->setFont(QFont("San Francisco Display Thin", 13));
+	//_Title->setScaledContents(true);
+
+	_primary->addWidget(_Title);
+	_primary->addWidget(_minimize);
+	_primary->addWidget(_maxmimize);
+	_primary->addWidget(_close);
+	_primary->setMargin(0);
+	_primary->setSpacing(0);
+	setLayout(_primary);
+}
+
+void TitleBar::setIcon(const QPixmap &_pixelmap) {
+	if (_hastext)
+	{
+		delete _primary;
+	}
+	else
+	{
+		delete _l;
+	}
+	_hasicon = 1;
+	_window_icon->setPixmap(_pixelmap);
+	_secoundry->addWidget(_window_icon);
+	_secoundry->addWidget(_Title);
+	_secoundry->addWidget(_minimize);
+	_secoundry->addWidget(_maxmimize);
+	_secoundry->addWidget(_close);
+	_secoundry->setMargin(0);
+	_secoundry->setSpacing(0);
+	setLayout(_secoundry);
+}
+
+qint16 TitleBar::minimumwidth() const {
+	qint16 minimumwidth = (_close->width()) * 3 + (_maxmimize->width()) * 3 + (_minimize->width()) * 3;
+	if (_hastext)
+	{
+		minimumwidth += _textwidth;
+	}
+	if (_hasicon)
+	{
+		minimumwidth += _window_icon->width();
+	}
+	return minimumwidth;
+}
 
 void TitleBar::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
@@ -19,42 +79,42 @@ void TitleBar::paintEvent(QPaintEvent *) {
 	painter.drawRect(0, 0, width(), height());
 }
 
-void TitleBar::setText(const QString &_text) {
-	delete _l;
-	_hastext = 1;
-	_Title->setText(_text);
-	_Title->setMinimumHeight((_width / 2) + 3);
-	_Title->setStyleSheet("color:white;");
-	_Title->setFont(QFont("San Francisco Display Thin", (_width / 2) - 3));
-
-	_primary->addWidget(_Title);
-	_primary->addStretch(10);
-	_primary->addWidget(_minimize);
-	_primary->addWidget(_maxmimize);
-	_primary->addWidget(_close);
-	_primary->setMargin(10);
-	_primary->setSpacing((_width / 2) - 2);
-	setLayout(_primary);
-}
-
-void TitleBar::setIcon(const QPixmap &_pixelmap) {
-	if (!_hastext)
+void TitleBar::mousePressEvent(QMouseEvent *e) {
+	if (e->type() == QMouseEvent::MouseButtonPress && e->button() == Qt::LeftButton)
 	{
-		delete _l;
+	e->accept();
+	_start = e->globalPos() - _parent->frameGeometry().topLeft();
 	}
-	delete _primary;
-	_window_icon->setPixmap(_pixelmap);
-	_window_icon->setFixedSize((_width / 2) + 3, (_width / 2) + 3);
-	_window_icon->setScaledContents(true);
-
-	_secoundry->addWidget(_window_icon);
-	_secoundry->addWidget(_Title);
-	_secoundry->addStretch(10);
-	_secoundry->addWidget(_minimize);
-	_secoundry->addWidget(_maxmimize);
-	_secoundry->addWidget(_close);
-	_secoundry->setMargin(10);
-	_secoundry->setSpacing((_width / 2) - 2);
-	setLayout(_secoundry);
 }
+
+void TitleBar::mouseMoveEvent(QMouseEvent *e) {
+	e->accept();
+	_delta = e->globalPos() - _start;
+	_parent->move(x() + _delta.x(), y() + _delta.y());
+}
+
+void TitleBar::mouseDoubleClickEvent(QMouseEvent *e) {
+	if (e->type() == QMouseEvent::MouseButtonDblClick && e->button() == Qt::LeftButton)
+	{
+		e->accept();
+		changeState();
+	}
+}
+
+void TitleBar::changeState() {
+	if (_maximized)
+	{
+		_maximized = 0;
+		_maxmimize->setMaximized(0);
+		_parent->restoreGeometry(_state);
+	}
+	else
+	{
+		_maximized = 1;
+		_maxmimize->setMaximized(1);
+		_state = _parent->saveGeometry();
+		_parent->showMaximized();
+	}
+}
+
 
