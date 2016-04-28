03e06c967fd49f9c8d363b8ca1aba100f2a366ae
Iman <iman72411@yahoo.com>
2016-04-28 20:23:06 +0430

    Update NcFramelessHelper.h

--- Ui/Qt5.5.1/NcFramelessHelper.h
+++ Ui/Qt5.5.1/NcFramelessHelper.h
@@ -1,25 +1,3 @@
-/*
-    NcFramelessHelper, an easy way to support move/resize on
-    frameless toplevel windows.
-    Copyright (C) 2011  Nishant Parashar
-    Email:- nishcode (at) gmail (dot) com
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-//Version 3.0.0
-
 #ifndef NC_FRAMELESS_HELPER_H
 #define NC_FRAMELESS_HELPER_H
 
@@ -49,12 +27,6 @@ public:
   void useRubberBandOnResize( bool use );
   bool isUsingRubberBandOnResisze();
 
-  //Make sure to leave the same content margins
-  //around the widget as the newBorderWidth
-  //this can be done by
-  //yourWidget->layout()->setMargin( newBorderWidth );
-  //otherwise your widget will not expose the
-  //area where this class works
   void setBorderWidth( int newBorderWidth );
   int borderWidth();
 
