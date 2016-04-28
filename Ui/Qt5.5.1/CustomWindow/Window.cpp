0fc7e79236ded5a0c6678dc3d650a91911d4fd96
Iman <iman72411@yahoo.com>
2016-04-28 18:09:22 +0430

    Update Window.cpp

--- Ui/Qt5.5.1/CustomWindow/Window.cpp
+++ Ui/Qt5.5.1/CustomWindow/Window.cpp
@@ -1,9 +1,9 @@
-#include "customwindow.h"
-
 Window::Window(QWidget *parent)
 	:QWidget(parent) {
-	setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
-	setMinimumWidth(_titlebar->minimumwidth());
+	setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+	setAttribute(Qt::WA_TranslucentBackground);
+	setMinimumSize(640, 480);
+	setMouseTracking(true);
 	// _layout Settings
 	_layout->setMargin(0);
 	_layout->setSpacing(0);
@@ -14,9 +14,11 @@ Window::Window(QWidget *parent)
 
 Window::Window(const QString &_title, QWidget *parent)
 :QWidget(parent) {
-	setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+	setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+	setAttribute(Qt::WA_TranslucentBackground);
+	setMinimumSize(640, 480);
+	setMouseTracking(true);
 	_titlebar->setText(_title);
-	setMinimumWidth(_titlebar->minimumwidth());
 	// _layout Settings
 	_layout->setMargin(0);
 	_layout->setSpacing(0);
@@ -35,5 +37,10 @@ void Window::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
 	painter.setBrush(QColor("#FFFFFF"));
 	painter.setPen(Qt::NoPen);
-	painter.drawRect(0, 0, width(), height());
+	painter.setRenderHint(QPainter::Antialiasing);
+
+	QPainterPath path;
+	path.setFillRule(Qt::WindingFill);
+	path.addRoundedRect(QRect(0, 0, width(), height()), 5.0, 5.0);
+	painter.drawPath(path.simplified());
 }
