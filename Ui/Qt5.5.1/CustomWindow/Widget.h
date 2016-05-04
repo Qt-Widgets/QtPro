9f6bf93a1945ee691919b9b7872d609701e96cc4
Iman <iman72411@yahoo.com>
2016-05-04 12:55:24 +0430

    Update Widget.h

--- Ui/Qt5.5.1/CustomWindow/Widget.h
+++ Ui/Qt5.5.1/CustomWindow/Widget.h
@@ -1,3 +1,13 @@
+#pragma once
+#ifndef WIDGET_H
+#define WIDGET_H
+#include <QtWidgets>
+
+class QWidget;
+class QPainter;
+class QpainterPath;
+class QRubberBand;
+
 class Widget : public QWidget {
 	Q_OBJECT
 
@@ -21,16 +31,23 @@ private:
 	};
 	QRubberBand *_rubberband;
 	bool _cursorchanged;
+	bool _leftButtonPressed;
 	qint16 _borderWidth;
 	Edge _edge;
 
 private:
-	void hoverMoveEvent(QHoverEvent *e);
-	void leaveMoveEvent(QEvent *e);
+	void mouseHover(QHoverEvent *e);
+	void mouseLeave(QEvent *e);
+	void mousePress(QMouseEvent *e);
+	void mouseRealese(QMouseEvent *e);
+	void mouseMove(QMouseEvent *e);
 	void updateCursorShape(const QPoint &pos);
 	void calculateCursorPosition(const QPoint &pos);
+	void updateRubberBand();
 
 protected:
 	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
 	bool eventFilter(QObject *o, QEvent *e) Q_DECL_OVERRIDE;
 };
+
+#endif // !WIDGET_H
