cd2215f42ade7d5db9e2b54945ee3808d35b7530
Iman <iman72411@yahoo.com>
2016-05-07 14:23:44 +0430

    Update Widget.h

--- Ui/Qt5.5.1/CustomWindow/Widget.h
+++ Ui/Qt5.5.1/CustomWindow/Widget.h
@@ -1,13 +1,3 @@
-#pragma once
-#ifdef Q_OS_WIN
-
-#include <QtWidgets>
-
-class QWidget;
-class QPainter;
-class QpainterPath;
-class QRubberBand;
-
 class Widget : public QWidget {
 	Q_OBJECT
 
@@ -53,5 +43,3 @@ protected:
 	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
 	bool eventFilter(QObject *o, QEvent *e) Q_DECL_OVERRIDE;
 };
-
-#endif
