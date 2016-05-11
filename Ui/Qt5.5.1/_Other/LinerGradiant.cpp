3169d6dc6cb769a8683d8b7ac300186d079ea60b
Iman <iman72411@yahoo.com>
2016-05-11 15:47:56 +0430

    Update LinerGradiant.cpp

--- Ui/Qt5.5.1/_Other/LinerGradiant.cpp
+++ Ui/Qt5.5.1/_Other/LinerGradiant.cpp
@@ -1,63 +1,22 @@
-void Widget::paintEvent(QPaintEvent *) {
-	QPainter painter(this);
+QPainter painter(this);
 	painter.setRenderHint(QPainter::Antialiasing);
 	painter.setPen(Qt::NoPen);
 
 
 	QLinearGradient gradient;
-	gradient.setStart(0, 0);
-	gradient.setFinalStop(width(), 0);
+	QPointF right0(width() - 7, height() / 2);
+	QPointF right1(width(), height() / 2);
 
-	QColor grey1(150, 150, 150, 125);// starting color of the gradient - can play with the starting color and ,point since its not visible anyway
+	gradient.setStart(right0);
+	gradient.setFinalStop(right1);
 
-	// grey2 is ending color of the gradient - this is what will show up as the shadow. the last parameter is the alpha blend, its set
-	// to 125 allowing a mix of th color and and the background, making more realistic shadow effect.
-	QColor grey2(225, 225, 225, 125);
-
-	gradient.setColorAt(0.0, grey1);
-	gradient.setColorAt(0.1, grey2);
-
-	QBrush brush(gradient);
-	painter.setBrush(brush);
-
-	// for the desired effect, no border will be drawn, and because a brush was set, the drawRoundRect will fill the box with the gradient brush.
-
-	QPointF topLeft(10, 10);
-	QPointF bottomRight(width(), height());
-	QRectF rect(topLeft, bottomRight);
-	painter.drawRoundRect(rect, 0.0, 0.0); // corner radius of 25 pixels
-
-	// draw the top box, the visible one
-	QBrush brush2(QColor(255, 250, 200, 255), Qt::SolidPattern);
-	painter.setBrush(brush2);
-
-	QPointF topLeft2(0, 0);
-	QPointF bottomRight2(width() - 10, height() - 10);
-	QRectF rect2(topLeft2, bottomRight2);
-	painter.drawRoundRect(rect2, 0.0, 0.0);
-}
-
-
-
-
-
-
-
-
-void Widget::paintEvent(QPaintEvent *) {
-	QPainter painter(this);
-	painter.setRenderHint(QPainter::Antialiasing);
-	painter.setPen(Qt::NoPen);
-
-
-	QLinearGradient gradient(QPointF(0, 0), QPointF(width(), height()));
 	gradient.setSpread(QGradient::ReflectSpread);
 
-	QColor grey1(237, 237, 237, 64);// #EDEDED    orignal : 150, 150, 150, 125
-	QColor grey2(255, 255, 255, 0); // #FDFDFD   original : 225, 225, 225, 125
+	QColor grey1(151, 151, 151, 32);// #D8D8D8 
+	QColor grey2(254, 254, 254, 0); // #FEFEFE
 
-	gradient.setColorAt(0.0, grey1);
-	gradient.setColorAt(1.0, grey2);
+	gradient.setColorAt(0.000, grey1);
+	gradient.setColorAt(0.999, grey2);
 
 	painter.setBrush(QBrush(gradient));
 
@@ -70,40 +29,7 @@ void Widget::paintEvent(QPaintEvent *) {
 	QBrush brush2(QColor("#FFFFFF"), Qt::SolidPattern);
 	painter.setBrush(brush2);
 
-	QPointF topLeft2(5, 5);
-	QPointF bottomRight2(width() - 5, height() - 5);
+	QPointF topLeft2(0, 0);
+	QPointF bottomRight2(width()-7, height() );
 	QRectF rect2(topLeft2, bottomRight2);
-	//painter.drawRoundRect(rect2, 0.0, 0.0);
-}
-
-
-
-
-
-
-
-
-
-
-QPainter painter(this);
-	painter.setBrush(QColor("#FFFFFF"));
-	painter.setPen(Qt::NoPen);
-	painter.setRenderHint(QPainter::Antialiasing);
-
-	QPainterPath path;
-	path.addRoundedRect(QRect(0, 0, width(), height()), _radius, _radius);
-	painter.drawPath(path.simplified());
-
-	QLinearGradient grad;
-	QColor grey1(150, 150, 150, 125);
-	QColor grey2(225, 225, 225, 125);
-	grad.setStart(0, 0);
-	grad.setFinalStop(width(), 0);
-	grad.setColorAt(0.0, grey1);
-	grad.setColorAt(0.1, grey2);
-	painter.setBrush(QBrush(grad));
-	QPointF top(3, 3);
-	QPointF bottom(width(), height());
-	painter.setPen(Qt::NoPen);
-	painter.setRenderHint(QPainter::Antialiasing);
-	painter.drawRoundRect(QRectF(top, bottom), _radius, _radius);
+	painter.drawRoundRect(rect2, 0.0, 0.0);
