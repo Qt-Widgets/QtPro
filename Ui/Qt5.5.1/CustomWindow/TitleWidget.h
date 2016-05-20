fd2b8f04ae3eabd9a16286839b65c146782659ae
Iman <iman72411@yahoo.com>
2016-05-20 21:08:32 +0430

    Update TitleWidget.h

--- Ui/Qt5.5.1/CustomWindow/TitleWidget.h
+++ Ui/Qt5.5.1/CustomWindow/TitleWidget.h
@@ -1,65 +1,141 @@
+/*
+CustomWindow is a simple and easy to use custom top-level window (for Windows OS) created by Qt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details
+Full License:https://github.com/QtTools/CustomWindow/blob/master/LICENSE
+
+Copyright (c) 2016 Iman Ahmadvand (IMAN4K), Contact:iman72411@yahoo.com
+*/
+
 #pragma once
-#ifndef TITLEWIDGET_H
-#define TITLEWIDGET_H
 
-#include <QtWidgets>
+#include <QWidget>
+#include <QPainter>
+#include <QTimer>
+#include <QMouseEvent>
+#include <QPixmap>
+#include <QLine>
+#include <QRect>
+#include <QPen>
 
-class QWidget;
-class QPainter;
-class QPainterPath;
-class QTimer;
-
-enum Type
-{
-	Close,
-	Minimize,
-	Maxmimize,
-	Icon
-};
 
 class TitleWidget : public QWidget
 {
 	Q_OBJECT
 
-private:
-	enum State
-	{
-		Normal,
-		Disable,
-		Hover,
-		Over
-	};
-	State _state;
-	Type _type ;
-	QColor _color;
-	qint16 _weight;
-	qint16 _size;
-	qreal _opacity;
-	qreal _step;
-	QTimer *_timer = new QTimer();
-	bool _maxmized;
-	QPixmap _pixelmap;
-
 public:
-	explicit TitleWidget(Type _tp, QWidget *parent = 0);
+	explicit TitleWidget(QWidget *parent = 0);
 	~TitleWidget();
-	void setMaximized(const bool _control);
-	void setPixmap(const QPixmap _pixmap);
+	void setDisable(bool disable);
+	bool isDisable() const;
+	void setSize(const qint16 &size);
 
 protected:
-	virtual void paintEvent(QPaintEvent *);
-	virtual void enterEvent(QEvent *);
-	virtual void leaveEvent(QEvent *);
-	virtual void mousePressEvent(QMouseEvent *e);
-	virtual QSize minimumSizeHint() const;
-	virtual QSize sizeHint() const;
+	enum State {
+		None = 0x00,
+		Disable = 0x01,
+		Hover = 0x02,
+		Over = 0x03
+	};
+	State _state;
+	qint16 _size;
+	qint16	_rait;
+	QColor _color;
+	qreal _weight;
+	qreal _opacity;
+	qreal _step;
+	QTimer _timer;
+
+protected:
+	void mousePressEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	void enterEvent(QEvent *) Q_DECL_OVERRIDE;
+	void leaveEvent(QEvent *) Q_DECL_OVERRIDE;
+	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+	QSize sizeHint() const Q_DECL_OVERRIDE;
 
 	signals:
 	void clicked();
 
 public slots:
 	void timercall();
-
 };
 
-#endif // !TITLEWIDGET_H
+
+class CloseButton : public TitleWidget {
+
+public:
+	explicit CloseButton(const qint16 &size);
+	~CloseButton();
+
+private:
+	QLine _line0;
+	QLine _line1;
+	QLine _line2;
+	QVector<QLine> _cross;
+
+protected:
+	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
+};
+
+
+class MaximizeButton : public TitleWidget {
+public:
+	explicit MaximizeButton(const qint16 &size);
+	~MaximizeButton();
+	void setMaximized(bool set);
+
+private:
+	bool _maximized;
+	QLine _line0;
+	QLine _line1;
+	QLine _line2;
+	QLine _line3;
+	QLine _line4;
+	QLine _line5;
+	QLine _line6;
+	QLine _line7;
+	QVector<QLine> _maximize;
+	QVector<QLine> _restore;
+
+protected:
+	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
+};
+
+
+class MinimizeButton : public TitleWidget {
+
+public:
+	explicit MinimizeButton(const qint16 &size);
+	~MinimizeButton();
+
+private:
+	QLine _line0;
+protected:
+	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
+};
+
+
+class Icon : public QWidget {
+	Q_OBJECT
+
+public:
+	explicit Icon();
+	~Icon();
+	void setPixmap(const QPixmap &_pixmap, const qint16 &_width);
+
+private:
+	QPixmap _pixelmap;
+	QRect _rect;
+	qint16 _size;
+
+protected:
+	void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
+};
