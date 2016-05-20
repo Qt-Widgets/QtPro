56e1c540c9a3ddfef512d218a11c63c382b9f97c
Iman <iman72411@yahoo.com>
2016-05-20 21:39:33 +0430

    Update Shadow.h

--- Ui/Qt5.5.1/CustomWindow/Shadow.h
+++ Ui/Qt5.5.1/CustomWindow/Shadow.h
@@ -1,11 +1,28 @@
-#include <QtWidgets>
+/*
+CustomWindow is a simple and easy to use custom top-level window (for Windows OS) created by Qt
 
-class QPainter;
-class QPointF;
-class QRectF;
-class QColor;
-class QBrush;
-class QLinerGradient;
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details
+Full License:https://github.com/QtTools/CustomWindow/blob/master/LICENSE
+
+Copyright (c) 2016 Iman Ahmadvand (IMAN4K), Contact:iman72411@yahoo.com
+*/
+
+#include <QPainter>
+#include <QPointF>
+#include <QRectF>
+#include <QColor>
+#include <QBrush>
+#include <QPainterPath>
+
+#if defined Q_OS_WIN
 
 void drawShadow(QPainter &_painter, const qint16 _margin, const qreal _radius, const QColor _start, const QColor _end,
 	const qreal _startPosition, const qreal _endPosition0, const qreal _endPosition1, const qreal _width, const qreal _height) {
@@ -21,14 +38,22 @@ void drawShadow(QPainter &_painter, const qint16 _margin, const qreal _radius, c
 	gradient.setStart(right0);
 	gradient.setFinalStop(right1);
 	_painter.setBrush(QBrush(gradient));
-	_painter.drawRoundRect(QRectF(QPointF(_width - _margin*_radius, _margin), QPointF(_width, _height - _margin)), 0.0, 0.0);
+	if (_radius == 0) {
+		_painter.drawRoundRect(QRectF(QPointF(_width - _margin, _margin), QPointF(_width, _height - _margin)), 0.0, 0.0);
+	} else {
+		_painter.drawRoundRect(QRectF(QPointF(_width - _margin*_radius, _margin), QPointF(_width, _height - _margin)), 0.0, 0.0);
+	}
 	// Left
 	QPointF left0(_margin, _height / 2);
 	QPointF left1(0, _height / 2);
 	gradient.setStart(left0);
 	gradient.setFinalStop(left1);
 	_painter.setBrush(QBrush(gradient));
-	_painter.drawRoundRect(QRectF(QPointF(_margin *_radius, _margin), QPointF(0, _height - _margin)), 0.0, 0.0);
+	if (_radius == 0) {
+		_painter.drawRoundRect(QRectF(QPointF(_margin, _margin), QPointF(0, _height - _margin)), 0.0, 0.0);
+	} else {
+		_painter.drawRoundRect(QRectF(QPointF(_margin*_radius, _margin), QPointF(0, _height - _margin)), 0.0, 0.0);
+	}
 	// Top
 	QPointF top0(_width / 2, _margin);
 	QPointF top1(_width / 2, 0);
@@ -77,8 +102,11 @@ void drawShadow(QPainter &_painter, const qint16 _margin, const qreal _radius, c
 	_painter.drawRoundRect(QRectF(topright0, topright1), 0.0, 0.0);
 	// Widget
 	_painter.setBrush(QBrush("#FFFFFF"));
-	_painter.setRenderHint(QPainter::Antialiasing);
+	_painter.setRenderHint(QPainter::HighQualityAntialiasing);
 	QPainterPath path;
+	path.setFillRule(Qt::WindingFill);
 	path.addRoundedRect(QRectF(QPointF(_margin, _margin), QPointF(_width - _margin, _height - _margin)), _radius, _radius);
 	_painter.drawPath(path.simplified());
 }
+
+#endif
