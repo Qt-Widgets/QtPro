c11bb1e70f11cc4a4021b99870707df7d21c32e6
Iman <iman72411@yahoo.com>
2016-05-27 20:10:00 +0430

    TitleBar's Widgets

--- Ui/Qt5.5.1/CustomWindow/TitleWidget.cpp
+++ Ui/Qt5.5.1/CustomWindow/TitleWidget.cpp
@@ -1,5 +1,5 @@
 /*
-CustomWindow is a simple and easy to use custom top-level window (for Windows OS) created by Qt
+CustomWindow is a simple, native and easy to use custom top-level window created by Qt
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,7 +17,10 @@ Copyright (c) 2016 Iman Ahmadvand (IMAN4K), Contact:iman72411@yahoo.com
 
 #include "TitleWidget.h"
 
-TitleWidget::TitleWidget(QWidget *parent) :QWidget(parent), _state(None), _color("#FFFFFF"), _opacity(0.650), _step(0.004)
+TitleWidget::TitleWidget(QWidget *parent) :QWidget(parent),
+_state(None),
+_opacity(0.650),
+_step(0.004)
 {
 	setAttribute(Qt::WA_StaticContents);
 	setMouseTracking(true);
@@ -62,6 +65,7 @@ QSize TitleWidget::sizeHint() const {
 void TitleWidget::setDisable(bool disable) {
 	if (disable) {
 		_state = Disable;
+		_opacity = 0.4;
 	} else {
 		_state = None;
 	}
@@ -81,15 +85,7 @@ void TitleWidget::setSize(const qint16 &size) {
 	_size = size;
 	_rait = (sqrt((size*size) / 8) + 1) / 2;
 	_weight = size / 24.0;
-}
-
-void TitleWidget::setColor(const QColor &color) {
-	_color = color;
-	update();
-}
-
-QColor TitleWidget::color() const {
-	return _color;
+	_pen = QPen(QBrush("#FFFFFF", Qt::SolidPattern), _weight, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
 }
 
 void TitleWidget::timercall() {
@@ -132,32 +128,29 @@ CloseButton::~CloseButton() {}
 void CloseButton::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
 	painter.setRenderHint(QPainter::Antialiasing);
-	painter.setPen(QPen(_color, _weight, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	painter.setPen(_pen);
 	painter.setOpacity(_opacity);
-	if (_state == Disable) {
-		_color = "#CCCCCC";
-	}
 	painter.drawLines(_cross);
 }
 
 
 MaximizeButton::MaximizeButton(const qint16 &size) : _maximized(false) {
 	setSize(size);
-	_line0 = QLine((size / 2) - _rait, (size / 2) - _rait + _weight - (_weight / 3), (size / 2) - _rait, (size / 2) + _rait);
-	_line1 = QLine((size / 2) - _rait + _weight - (_weight / 3), (size / 2) + _rait, (size / 2) + _rait, (size / 2) + _rait);
+	_line0 = QLine((size / 2) - _rait, (size / 2) - _rait + _weight, (size / 2) - _rait, (size / 2) + _rait);
+	_line1 = QLine((size / 2) - _rait + _weight, (size / 2) + _rait, (size / 2) + _rait, (size / 2) + _rait);
 	_line2 = QLine((size / 2) + _rait, (size / 2) + _rait - _weight + (_weight / 3), (size / 2) + _rait, (size / 2) - _rait);
 	_line3 = QLine((size / 2) + _rait - _weight + (_weight / 3), (size / 2) - _rait, (size / 2) - _rait, (size / 2) - _rait);
 	_maximize << _line0 << _line1 << _line2 << _line3;
 	// inner
-	_line0 = QLine((size / 2) - _rait, (size / 2) - _weight - (_weight / 3), (size / 2) - _rait, (size / 2) + _rait);
-	_line1 = QLine((size / 2) - _rait + _weight - (_weight / 3), (size / 2) + _rait, (size / 2) + (_weight * 2), (size / 2) + _rait);
-	_line2 = QLine((size / 2) + (_weight * 2), (size / 2) + _rait - (_weight / 2), (size / 2) + (_weight * 2), (size / 2) - _weight - (_weight / 3));
+	_line0 = QLine((size / 2) - _rait, (size / 2) - _weight, (size / 2) - _rait, (size / 2) + _rait);
+	_line1 = QLine((size / 2) - _rait + _weight, (size / 2) + _rait, (size / 2) + _weight + (_weight / 3), (size / 2) + _rait);
+	_line2 = QLine((size / 2) + (_weight * 2), (size / 2) + _rait - (_weight / 2), (size / 2) + (_weight * 2), (size / 2) - _weight);
 	_line3 = QLine((size / 2) + (_weight * 2), (size / 2) - (_weight * 2), (size / 2) - _rait, (size / 2) - (_weight * 2));
 	// outer
-	_line4 = QLine((size / 2) - (_weight * 2), (size / 2) - (_weight * 2) - (_weight / 2), (size / 2) - (_weight * 2), (size / 2) - _rait);
-	_line5 = QLine((size / 2) - _weight - (_weight / 3), (size / 2) - _rait, (size / 2) + _rait + (_weight / 2), (size / 2) - _rait);
-	_line6 = QLine((size / 2) + _rait + (_weight / 2), (size / 2) - _rait + (_weight / 3) + (_weight / 2), (size / 2) + _rait + (_weight / 2), (size / 2) + _weight + (_weight / 2));
-	_line7 = QLine((size / 2) + _rait + (_weight / 2), (size / 2) + (_weight * 2), (size / 2) + (_weight * 3) - (_weight / 3), (size / 2) + (_weight * 2));
+	_line4 = QLine((size / 2) - (_weight * 2), (size / 2) - _weight - (_weight * 2), (size / 2) - (_weight * 2), (size / 2) - _rait);
+	_line5 = QLine((size / 2) - _weight, (size / 2) - _rait, (size / 2) + _rait + (_weight / 2), (size / 2) - _rait);
+	_line6 = QLine((size / 2) + _rait + (_weight / 2), (size / 2) - _rait + (_weight / 3) + (_weight / 2), (size / 2) + _rait + (_weight / 2), (size / 2) + _weight + (_weight / 3));
+	_line7 = QLine((size / 2) + _rait + (_weight / 2), (size / 2) + (_weight * 2), (size / 2) + (_weight * 3) + (_weight / 2), (size / 2) + (_weight * 2));
 	_restore << _line0 << _line1 << _line2 << _line3 << _line4 << _line5 << _line6 << _line7;
 }
 
@@ -166,21 +159,18 @@ MaximizeButton::~MaximizeButton() {}
 void MaximizeButton::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
 	painter.setRenderHint(QPainter::Antialiasing);
-	painter.setPen(QPen(_color, _weight, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	painter.setPen(_pen);
 	painter.setOpacity(_opacity);
 	if (_maximized) {
 		painter.drawLines(_restore);
 	} else {
-		if (_state == Disable) {
-			_color = "#CCCCCC";
-		}
 		painter.drawLines(_maximize);
 	}
 }
 
 void MaximizeButton::setMaximized(bool set) {
 	_maximized = set;
-	update();
+	repaint();
 }
 
 
@@ -193,12 +183,9 @@ MinimizeButton::~MinimizeButton() {}
 
 void MinimizeButton::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
-	painter.setRenderHint(QPainter::HighQualityAntialiasing);
-	painter.setPen(QPen(_color, _weight, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	painter.setRenderHint(QPainter::Antialiasing);
+	painter.setPen(_pen);
 	painter.setOpacity(_opacity);
-	if (_state == Disable) {
-		_color = "#CCCCCC";
-	}
 	painter.drawLine(_line0);
 }
 
