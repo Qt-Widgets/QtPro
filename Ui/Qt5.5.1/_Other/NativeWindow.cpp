e17f8c7ab1bde2920ba05760491afc54722a5bd4
Iman <iman72411@yahoo.com>
2016-05-30 11:02:09 +0430

    Add files via upload

---
+++ Ui/Qt5.5.1/_Other/NativeWindow.cpp
@@ -0,0 +1,64 @@
+#include <windows.h>
+
+LPCWSTR Class_Name_Size = L"Win32APP";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hwnd); break;
+	case WM_DESTROY:
+		PostQuitMessage(0); break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+		break;
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInsance, HINSTANCE hPrevInsance, LPSTR lpCmdLine, int nCmdShow) {
+	WNDCLASSEX wcex;
+	HWND hwnd;
+	MSG msg;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = 0;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0; 
+	wcex.hInstance = hInsance;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = Class_Name_Size;
+	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex)) {
+		MessageBox(NULL, (LPCWSTR)L"Window Registeration Faild !", (LPCWSTR)L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		Class_Name_Size,
+		(LPCWSTR)L"Sample Win32 APP",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+		NULL, NULL, hInsance, NULL);
+
+	if (hwnd == NULL) {
+		MessageBox(NULL, (LPCWSTR)L"Window Registeration Faild !", (LPCWSTR)L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, NULL, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
