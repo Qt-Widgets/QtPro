5f6ccc62e1834d6c98e3e15fb03d6b19ce9abe13
Iman <iman72411@yahoo.com>
2016-06-16 12:47:55 +0430

    Update Button.h

--- Ui/Qt5.5.1/Button.h
+++ Ui/Qt5.5.1/Button.h
@@ -7,27 +7,137 @@ class Button :public QWidget
 	Q_OBJECT
 
 public:
-	Button();
+	explicit Button(QWidget *parent = 0);
+	explicit Button(const QString &text, const QColor &color, QWidget *parent = 0);
 	~Button();
 	void setText(const QString &text);
+	void setColor(const QColor &color);
 	void setDisable(bool _disable);
+	void setFont(const QString &family, const qint16 &pointsize);
 	bool isDisable() const;
+	QString text() const;
+	QColor color() const;
 
-private:
+protected:
 	enum State {
 		None = 0x00,
 		Hover = 0x02,
 		Over = 0x04,
-		Disabe = 0x08
+		Pressed = 0x08,
+		Released = 0x0B,
 	};
 
-	bool _disable;
+	bool _disabled;
+
 	State _state;
 
+	QString _text;
+	QString _fontfamily;
+
+	QColor _color;
+	QColor _brush;
+
+	qreal _opacity;
+	qreal _radius;
+
+	qint16 _height;
+	qint16 _padding;
+	qint16 _pointsize;
+	qint16 _x;
+	qint16 _y;
+
+	QFont _font;
+
+	QTimer _timer;
+
 protected:
+	void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	void mousePressEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	void enterEvent(QEvent *) Q_DECL_OVERRIDE;
+	void leaveEvent(QEvent *) Q_DECL_OVERRIDE;
 	QSize sizeHint() const Q_DECL_OVERRIDE;
 	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
-	void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+
+signals:
+	void clicked();
 };
 
-#endif // BUTTON_H
+
+class RaisedButton :public Button
+{
+	Q_OBJECT
+
+public:
+	explicit RaisedButton(QWidget *parent = 0);
+	explicit RaisedButton(const QString &text, QWidget *parent = 0);
+	explicit RaisedButton(const QString &text, const QColor &color, QWidget *parent = 0);
+	~RaisedButton();
+
+protected:
+	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
+
+	public slots:
+	void timercall();
+};
+
+
+class FlatButton :public Button
+{
+	Q_OBJECT
+
+public:
+	explicit FlatButton(QWidget *parent = 0);
+	explicit FlatButton(const QString &text, QWidget *parent = 0);
+	explicit FlatButton(const QString &text, const QColor &color, QWidget *parent = 0);
+	~FlatButton();
+
+private:
+	qint16 _blank;
+
+protected:
+	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
+	void mousePressEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+
+	public slots:
+	void timercall();
+};
+
+
+class IconButton :public Button
+{
+	Q_OBJECT
+
+public:
+	explicit IconButton(const QPixmap &icon, QWidget *parent = 0);
+	explicit IconButton(const QPixmap &icon, const QColor &brush, QWidget *parent = 0);
+
+private:
+	QPixmap _icon;
+	QRect _rect;
+	qint16 _rait;
+
+protected:
+	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
+	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+	void mousePressEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+
+	public slots:
+	void timercall();
+};
+
+
+class LinkButton :public Button
+{
+	Q_OBJECT
+
+public:
+	explicit LinkButton(QWidget *parent = 0);
+	explicit LinkButton(const QString &text, QWidget *parent = 0);
+protected:
+	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
+	void enterEvent(QEvent *) Q_DECL_OVERRIDE;
+	void leaveEvent(QEvent *) Q_DECL_OVERRIDE;
+	QSize minimumSizeHint() const;
+};
