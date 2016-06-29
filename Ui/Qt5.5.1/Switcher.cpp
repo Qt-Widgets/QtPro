645a0cca748f6d7856b920f7076512d05e43163b
Iman <iman72411@yahoo.com>
2016-06-29 09:46:10 +0430

    Update Switcher.cpp

--- Ui/Qt5.5.1/Switcher.cpp
+++ Ui/Qt5.5.1/Switcher.cpp
@@ -1,1 +1,62 @@
+#include "switcher.h"
 
+Switcher::Switcher(QWidget *parent) : QWidget(parent),
+_opacity(0.000),
+_x(0),
+_disabled(false),
+_switch(true),
+_radius(10.0)
+{
+
+}
+
+Switcher::Switcher(const QColor &color, QWidget *parent) : QWidget(parent),
+_disabled(false),
+_switch(true),
+_x(0),
+_opacity(0.000),
+_radius(10.0)
+{
+	_brush = color;
+}
+
+void Switcher::mousePressEvent(QMouseEvent *e) {
+	if (!_disabled) {
+		if (e->buttons() & Qt::LeftButton) {
+			e->accept();
+		} else {
+			e->ignore();
+		}
+	}
+}
+
+void Switcher::mouseReleaseEvent(QMouseEvent *e) {
+	if (!_disabled) {
+		if (e->type() == QMouseEvent::MouseButtonRelease && e->button() == Qt::LeftButton) {
+			e->accept();
+			emit toggled();
+			_switch = _switch ? false : true;
+		} else {
+			e->ignore();
+		}
+	}
+}
+
+QSize Switcher::sizeHint() const {
+	return minimumSizeHint();
+}
+
+QSize Switcher::minimumSizeHint() const {
+	return QSize(_height * 2, _height);
+}
+
+void Switcher::timercall() {
+	if (_switch)
+	{
+
+	}
+	else
+	{
+
+	}
+}
