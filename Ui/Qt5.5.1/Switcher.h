214e4212941f9cc30ad8515acdeedf54a6afe8e4
Iman <iman72411@yahoo.com>
2016-06-29 09:45:41 +0430

    Update Switcher.h

--- Ui/Qt5.5.1/Switcher.h
+++ Ui/Qt5.5.1/Switcher.h
@@ -1,5 +1,3 @@
-#include <QtWidgets>
-
 class Switcher : public QWidget
 {
 	Q_OBJECT
@@ -7,18 +5,30 @@ class Switcher : public QWidget
 public:
 	explicit Switcher(QWidget *parent = 0);
 	explicit Switcher(const QColor &color, QWidget *parent = 0);
+	void setColor(const QColor &color);
+	QColor color() const;
 
 private:
+	bool _disabled;
+	bool _switch;
 	QColor _brush;
 	qreal _opacity;
+	qreal _radius;
 	qint16 _x;
 	qint16 _y;
+	QTimer _timer;
+	qint16 _height;
 
 protected:
-	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
 	void mousePressEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
 	void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;
+	QSize sizeHint() const Q_DECL_OVERRIDE;
+	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+	void paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;
+
+	public slots:
+	void timercall();
 
 signals:
-	void switched();
+	void toggled();
 };
