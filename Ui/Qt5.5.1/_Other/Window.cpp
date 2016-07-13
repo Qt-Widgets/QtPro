779ebbbeb2d51ed73ffbd1a36aeb1dcff8dc5063
Iman <iman72411@yahoo.com>
2016-07-13 23:37:21 +0430

    Update Window.cpp

--- Ui/Qt5.5.1/_Other/Window.cpp
+++ Ui/Qt5.5.1/_Other/Window.cpp
@@ -6,54 +6,58 @@
 #pragma comment(lib, "dwmapi.lib")
 
 LPCWSTR CLASS_NAME = L"Win32APP";
-#define  BORDER_WIDTH  5
+#define BORDER_WIDTH 8
 
-enum Style : DWORD {
-	BorderLess = (WS_POPUP | WS_THICKFRAME | WS_CAPTION | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX)
+enum class Style : DWORD {
+	BorderLess = (WS_CAPTION | WS_VISIBLE | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX)
 };
-
-void HWND_deleter::operator()(HWND handle) const {
-	assert(DestroyWindow(handle));
-}
-
-
 
 const std::wstring& Window::registerWindow() {
 
 	static const std::wstring WINDOW_CLASS = []{
-		WNDCLASSEX wcx{};
-		wcx.cbSize = sizeof(wcx);
-		wcx.style = CS_HREDRAW | CS_VREDRAW;
-		wcx.hInstance = (HINSTANCE)GetModuleHandle(nullptr);
-		wcx.lpfnWndProc = &Window::WndProc;
-		wcx.lpszClassName = CLASS_NAME;
-		wcx.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
-		wcx.hCursor = LoadCursor((HINSTANCE)GetModuleHandle(nullptr), IDC_ARROW);
+		WNDCLASSEX wcex{};
 
-		const HRESULT result = RegisterClassEx(&wcx);
-		
+		wcex.cbSize = sizeof(WNDCLASSEX);
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = 0;
+		wcex.hInstance = (HINSTANCE)GetModuleHandle(nullptr);
+		wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+		wcex.lpszMenuName = NULL;
+		wcex.lpszClassName = CLASS_NAME;
+		wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+		const HRESULT result = RegisterClassEx(&wcex);
+
 		if (FAILED(result)) throw std::runtime_error("Faild To Register Window Class");
 
-		return wcx.lpszClassName;
+		return wcex.lpszClassName;
 	}();
 	return WINDOW_CLASS;
 }
 
 Window::Window()
-	: hWnd(CreateWindow(registerWindow().c_str(),
-	L"Borderless Window",
+	: hWnd(
+	CreateWindow(
+	registerWindow().c_str(),
+	NULL,
 	static_cast<DWORD>(Style::BorderLess),
-	0, 0, 480, 320,
-	nullptr, nullptr,
+	CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+	NULL, NULL,
 	(HINSTANCE)GetModuleHandle(nullptr),
-	nullptr)) 
+	NULL) ,&DestroyWindow)
 {
 	if (!hWnd) throw std::runtime_error("Failed To Create Window");
 
 	SetWindowLongPtr(hWnd.get(), GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
 
-	setBorderLess();
-	setShadow();
+	SetWindowPos(hWnd.get(), nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE);
+
+	removeBorder();
+
 	show();
 }
 
@@ -61,15 +65,18 @@ LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpar
 	if (Window* window_ptr = reinterpret_cast<Window*>(GetWindowLongPtr(hwnd, GWLP_USERDATA))) {
 		auto & window = *window_ptr;
 
-		switch (msg) 
+		switch (msg)
 		{
-		case WM_NCCALCSIZE: 
-			return 0;
-			break;
+		case WM_NCCALCSIZE: {
+			NCCALCSIZE_PARAMS *pncsp = reinterpret_cast<NCCALCSIZE_PARAMS*>(lparam);
 
-		case WM_NCACTIVATE:  
-			return 1;
-			break;
+			pncsp->rgrc[0].left = pncsp->rgrc[0].left + BORDER_WIDTH;
+			pncsp->rgrc[0].top = pncsp->rgrc[0].top + 0;
+			pncsp->rgrc[0].right = pncsp->rgrc[0].right - BORDER_WIDTH;
+			pncsp->rgrc[0].bottom = pncsp->rgrc[0].bottom - BORDER_WIDTH;
+
+			return 0;
+		} break;
 
 		case WM_NCHITTEST: {
 			RECT winrect;
@@ -116,31 +123,27 @@ LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpar
 			return HTCAPTION;
 		} break;
 
-		case WM_CLOSE: 
-			DestroyWindow(hwnd);
-			window._closed = true;
-			return 0;
-			break;
-
 		case WM_DESTROY:
+			window._closed = true;
 			PostQuitMessage(0);
 			return 0;
 		}
-		}
-	return DefWindowProc(hwnd, msg, wparam, lparam);
 	}
-
-void Window::setBorderLess() const {
-	SetWindowLongPtr(hWnd.get(), GWL_STYLE, static_cast<LONG>(Style::BorderLess));
-	SetWindowPos(hWnd.get(), nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE);
-	setShadow();
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-void Window::setShadow() const {
-	static const MARGINS shadow_state = { 1, 1, 1, 1 };
-	DwmExtendFrameIntoClientArea(hWnd.get(), &shadow_state);
+void Window::removeBorder() const {
+	static const MARGINS frame = { -1, -1, -1, -1 };
+	DwmExtendFrameIntoClientArea(hWnd.get(), &frame);
 }
 
 void Window::show() const {
 	ShowWindow(hWnd.get(), SW_SHOW);
+	MSG msg{};
+	while (!isClosed()) {
+		while (GetMessage(&msg, NULL, NULL, 0) > 0) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 }
