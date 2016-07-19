ed10b924da79d1c7c8e5647f00fcccff4fb7744f
Iman <iman72411@yahoo.com>
2016-07-20 01:36:02 +0430

    Update Window.h

--- Ui/Qt5.5.1/_Other/Window.h
+++ Ui/Qt5.5.1/_Other/Window.h
@@ -1,34 +1,36 @@
 #pragma once
-#include <Windows.h>
+#include "qt_windows.h"
 #include <QtWidgets/QApplication>
-
 
 class Window
 {
 public:
-	enum class Style : DWORD {
-		BorderLess = (WS_CAPTION | WS_VISIBLE | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX)
-	};
+    enum class Style : DWORD {
+        BorderLess = (WS_CAPTION | WS_VISIBLE | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX)
+    };
 
-	Window(QApplication *app, const int &x, const int &y, const int &width, const int &height);
-	bool isClosed() const { return _closed; };
-	void setMinimumSize(const int &width, const int &height);
-	int minimumHeight() const{ return _height; }
-	int minimumWidth() const{ return _width; }
-	void setNcHeight(const int &non_client_height);
-	int NcHeight() const{ return NC_HEIGHT; }
+    Window(QApplication *app, const int &x, const int &y, const int &width, const int &height);
+
+    bool isClosed() const {return _closed;}
+    int minimumWidth() const {return _mWidth;}
+    int minimumHeight() const {return _mHeight;}
+    int NcHeight() const {return NC_HEIGHT;}
+
+    void setMinimumSize(const int &mWidth, const int &mHeight);
+    void setNcHeight(const int &non_client_height);
+
+    void show();
+    void removeBorder();
 
 private:
-	HWND _hWnd;
-	QApplication *_app;
-	bool _closed = false;
-	int _width;
-	int _height;
-	int NC_HEIGHT;
+    HWND _hwnd;
+    QApplication *_app = NULL;
+    bool _closed = false;
+    int _mWidth;
+    int _mHeight;
+    int NC_HEIGHT;
 
-	static const std::wstring& registerWindow();
-	static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
-
-	void show() const;
-	void removeBorder() const;
+private:
+    static const std::wstring& registerWindow();
+    static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 };
